dc40ee53f4b9714f621e3f50188fa5b7f25e5a66
Alejandra <alejandrarodriguez2508@gmail.com>
2023-03-10 22:39:39 -0500

    Establecer como pagina principal el home del proyecto

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-//import Footer from "../../components/Footer/Footer";
-//import Header from "../../components/Header/Header";
-import Login from "../../components/Login/Login.js";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+//import Login from "../../components/Login/Login.js";
 
 function Home() {
   return (
     <div>
-      <Login/>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
